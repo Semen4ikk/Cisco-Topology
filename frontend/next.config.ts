ac423d38eed91c8b499d1f07a66510f4f16f4465
Semen Prokopets <semen2004vuz@yandex.ru>
2026-04-18 19:45:48 +0400

    настроил пути для фото

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
     output: 'export',
     images: { unoptimized: true },
     basePath: '/Cisco-Topology',
+    assetPrefix: '/Cisco-Topology/'
     /* config options here */
 };
 
